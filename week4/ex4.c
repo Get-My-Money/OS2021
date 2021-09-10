c29d5c0f5e75e0fe0cdc34888296cabfb36c84bb
Get-My-Money <markzakharoff@mail.ru>
2021-09-10 18:12:44 +0300

    Update ex4.c
    ага

--- week4/ex4.c
+++ week4/ex4.c
@@ -4,33 +4,37 @@
 #include <unistd.h>
 
 int main() {
-	char command[100];
-	char parameters[100];
 	char word[200];
-	printf("Write your command and parameter: \n");
-
-	while (1) {
+	char* parameters[100];
+	printf("Write your command and parameters: \n");
+	
+	while (1){
 		printf("$ ");
 		fgets(word, 200, stdin);
-		int s = 0;
-
-		while (word[s] != ' ') {
-			command[s] = word[s];
-			s++;
-		}
-
-		while (word[s] != '\n') {
-			parameters[s - strlen(command)] = word[s + 1];
-			s++;
-		}
+		for(int i = 0; i < 100; i++)
+			parameters[i] = NULL;
+		parameters[0] = (char*)malloc(100);
+		int c1 = 0, c2 = 0, i = 0;
+	
+		while (word[i] != '\n'){
+			if (word[i] != ' '){
+				parameters[c1][c2] = word[i];
+				c2++;
+			}
+			else{
+				parameters[c1][c2] = '\0';
+				c1++;
+				parameters[c1] = (char*)malloc(100);
+				c2 = 0;
+				}
+			i++;	
+			}
 
 		int PID = fork();
-		if (PID > 0) {
-			char* const arguments[] = { command, parameters, 0 };
-			char *env[] = { 0 };
-			execve(command, arguments, env);
+		if (PID == 0){
+		char *envs[] = {"/bin", (char*)0};
+		execve(parameters[0], parameters, envs);
 		}
-
 	}
 	return 0;
 }
