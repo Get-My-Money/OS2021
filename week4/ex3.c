c138e8f60be2eec00916d1b8d5cb0cf6b32c5958
Get-My-Money <markzakharoff@mail.ru>
2021-09-10 14:04:38 +0300

    да
    ага

--- week4/ex3.c
+++ week4/ex3.c
@@ -11,7 +11,6 @@ int main() {
     printf("$ ");
     scanf("%s", command);
     system(command);
-    printf("\n");
   }
   return 0;
 }
--- week4/ex4.c
+++ week4/ex4.c
@@ -1,22 +1,36 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
-	char command[150];
-	char parameter[150];
+int main() {
+	char command[100];
+	char parameters[100];
+	char word[200];
 	printf("Write your command and parameter: \n");
-	
-	printf("$ ");
-	scanf("%s", command);
-	scanf("%s", parameter);
-	int PID = fork();
 
-	if(PID > 0){
-		char* const arguments[] = {command, parameter, 0};
-    char *o[] = {0};
-		execve(command, arguments, o);
-		printf("\n");
+	while (1) {
+		printf("$ ");
+		fgets(word, 200, stdin);
+		int s = 0;
+
+		while (word[s] != ' ') {
+			command[s] = word[s];
+			s++;
+		}
+
+		while (word[s] != '\n') {
+			parameters[s - strlen(command)] = word[s + 1];
+			s++;
+		}
+
+		int PID = fork();
+		if (PID > 0) {
+			char* const arguments[] = { command, parameters, 0 };
+			char *env[] = { 0 };
+			execve(command, arguments, env);
+		}
+
 	}
 	return 0;
 }
